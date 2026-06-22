170237e20808b2dc5fdb5c8c8abd6a05d5fa91bb
sabari-63 <srinivassabari63@gmail.com>
2026-06-23 00:35:10 +0530

    Add files via upload

---
+++ ECommereceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.e_commerce.Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommereceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
